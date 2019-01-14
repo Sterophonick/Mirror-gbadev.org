35daf085c610677dd958e8808d4ab35d655d0f97
Sterophonick <34801996+Sterophonick@users.noreply.github.com>
2019-01-13 18:24:28 -0700

    Initial Commit
    dear god this is a lot

---
+++ GBADEV.ORG/gbadev/gbadev.org/demos5343.php
@@ -0,0 +1,195 @@
+
+<!DOCTYPE html>
+<HTML>
+
+<!-- Mirrored from gbadev.org/demos.php?listby=name&offset=150 by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 05 Jan 2019 23:07:44 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+	<TITLE>Gameboy Advance Development</TITLE>
+	<STYLE TYPE='text/css'>
+		<!--
+		a.menu:link     { font: 14pt verdana; color: #000000; text-decoration: none; }
+		a.menu:visited     { font: 14pt verdana; color: #000000; text-decoration: none; }
+		a.menu:visited:hover     { font: 14pt verdana; color: #000000; text-decoration: none; }
+		a:link          { font: bold 10pt verdana; color: #3333FF; text-decoration: none; }
+		a:visited       { font: bold 10pt verdana; color: #3333FF; text-decoration: none; }
+		a:visited:hover { font: bold 10pt verdana; color: #3333FF; text-decoration: none; }
+		// -->
+	</STYLE>
+	<LINK HREF='http://rss.gbadev.org/feed.php' REL='alternate' TITLE='GBADEV.ORG RSS' TYPE='application/rss+xml'>
+	<META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=iso-8859-1'>
+	<META HTTP-EQUIV='Content-Style-Type' CONTENT='text/css'>
+</HEAD>
+
+<BODY BGCOLOR='#FFFFFF' TEXT='#000000'>
+
+<TABLE BORDER='0' CELLSPACING='5' CELLPADDING='0' WIDTH='800'>
+	<TR>
+		<TD WIDTH='160'></TD>
+		<TD ALIGN='CENTER' BGCOLOR='#FFFFFF'><IMG SRC='gbadev.png' WIDTH='600' HEIGHT='54' ALT='logo'></TD>
+		<TD WIDTH='200'></TD>
+	</TR>
+	<TR>
+		<TD VALIGN='TOP' WIDTH='170' BGCOLOR='#FFFFFF'>
+		<TABLE WIDTH='165' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+	<FONT FACE='verdana' SIZE='2'><B>Menu</B></FONT><BR></TD>
+</TR>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#CDCDCD'>
+	<FONT FACE='verdana'>
+	<A HREF='index-2.html' CLASS='menu'>Main</A><BR>
+	<A HREF='about.html' CLASS='menu'>About</A><BR>
+	<A HREF='search.html' CLASS='menu'>Search</A><BR>
+	<A HREF='submit.html' CLASS='menu'>Submit News</A><BR>
+	<A HREF='archive.html' CLASS='menu'>News Archive</A><BR>
+	<A HREF='http://forum.gbadev.org/' CLASS='menu'>Forum</A><BR>
+	<A HREF='contact.html' CLASS='menu'>Contact Us</A><BR>
+	<br>
+	<A HREF='tools.html' CLASS='menu'>Tools</A><BR>
+	<A HREF='demos.html' CLASS='menu'>Games/Demos</A><BR>
+	<A HREF='sources.html' CLASS='menu'>Sourcecode</A><BR>
+	<A HREF='docs.html' CLASS='menu'>Documentation</A><BR>
+	<A HREF='hardware.html' CLASS='menu'>Hardware</A><BR>
+	<A HREF='developers.html' CLASS='menu'>Developers</A><BR>
+	</FONT></TD>
+</TR>
+</TABLE></TD>
+		<TD VALIGN='TOP' WIDTH='600' BGCOLOR='#FFFFFF'>
+		<TABLE WIDTH='600' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+		<TR>
+			<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+			<FONT FACE='verdana' SIZE='2'><B>Games/Demos</B></FONT><BR></TD>
+		</TR>
+		<TR>
+			<TD ALIGN='LEFT' BGCOLOR='#CDCDCD'>
+			<FONT FACE='verdana' SIZE='2'>
+
+151. <A HREF='demos36c6.html?showinfo=1392'>Dots</A> [Games - Puzzle]<BR>
+152. <A HREF='demos7f30.html?showinfo=1422'>DragonBasic Donkey Kong</A> [Games - Classic]<BR>
+153. <A HREF='demos7470.html?showinfo=549'>DragonBasic Space Invaders</A> [Games - Classic]<BR>
+154. <A HREF='demos9325.html?showinfo=1412'>Drum Sequencer</A> [Demos - Tech Demo]<BR>
+155. <A HREF='demosd2e5.html?showinfo=536'>Dunjunz (BBC Micro)</A> [Games - Classic]<BR>
+156. <A HREF='demosebd5.html?showinfo=478'>E3 Update</A> [Games - Misc]<BR>
+157. <A HREF='demos954a.html?showinfo=301'>Ebooblution - Version 0.1</A> [Games - Shootem Up]<BR>
+158. <A HREF='demos2853.html?showinfo=215'>EdSmash</A> [Games - Shootem Up]<BR>
+159. <A HREF='demos8144.html?showinfo=682'>Elements Of Darkness</A> [Games - Platform]<BR>
+160. <A HREF='demos81ad.html?showinfo=1389'>Eliminator GBA v0.91</A> [Games - Racing]<BR>
+161. <A HREF='demos334a.html?showinfo=609'>Endless Tower</A> [Demos - Tech Demo]<BR>
+162. <A HREF='demos4da1.html?showinfo=611'>Endless Tower Update 1</A> [Games - Shootem Up]<BR>
+163. <A HREF='demose7b0.html?showinfo=1432'>Entombed</A> [Games - Platform]<BR>
+164. <A HREF='demos3ffe.html?showinfo=699'>Ex:Machina</A> [Games - Fighting]<BR>
+165. <A HREF='demos79ae.html?showinfo=1256'>Ex:Machina Version Deux</A> [Games - Fighting]<BR>
+166. <A HREF='demos6e4f.html?showinfo=571'>Eyeborg Advance Demo</A> [Games - Puzzle]<BR>
+167. <A HREF='demos65a2.html?showinfo=1411'>Fader</A> [Demos - Tech Demo]<BR>
+168. <A HREF='demosc396.html?showinfo=331'>FakePlasma</A> [Demos - Tech Demo]<BR>
+169. <A HREF='demos9beb.html?showinfo=315'>Ferk</A> [Games - Racing]<BR>
+170. <A HREF='demos1485.html?showinfo=1354'>FF4 Advance- The Return of Golbez</A> [Games - RPG]<BR>
+171. <A HREF='demos3f35.html?showinfo=506'>FFVs</A> [Demos - Tech Demo]<BR>
+172. <A HREF='demos3e2b.html?showinfo=466'>Fight Demo</A> [Demos - Tech Demo]<BR>
+173. <A HREF='demos40e0.html?showinfo=250'>Fill It Up</A> [Games - Puzzle]<BR>
+174. <A HREF='demosfce9.html?showinfo=627'>Final Fantasy 2 GBA demo</A> [Games - RPG]<BR>
+175. <A HREF='demosae47.html?showinfo=640'>Final Fantasy 2 GBA demo 0.2</A> [Games - RPG]<BR>
+176. <A HREF='demos628a.html?showinfo=1329'>Final Fantasy 4th World Cup won by Italy</A> [Demos - Tech Demo]<BR>
+177. <A HREF='demos4906.html?showinfo=561'>Fire Cube</A> [Demos - Tech Demo]<BR>
+178. <A HREF='demos5fbb.html?showinfo=563'>Fire Cube V2</A> [Demos - Tech Demo]<BR>
+179. <A HREF='demos95bb.html?showinfo=313'>Fireworks</A> [Demos - Tech Demo]<BR>
+180. <A HREF='demosff35.html?showinfo=1427'>Flappy Bird v1.0</A> [Games - Platform]<BR>
+181. <A HREF='demos6f91.html?showinfo=1386'>Flood GBA v0.9</A> [Games - Platform]<BR>
+182. <A HREF='demos7cb7.html?showinfo=522'>Flower</A> [Demos - Tech Demo]<BR>
+183. <A HREF='demos94c4.html?showinfo=249'>FORTH Cube</A> [Demos - Tech Demo]<BR>
+184. <A HREF='demosa67c.html?showinfo=351'>Fr018</A> [Demos - Tech Demo]<BR>
+185. <A HREF='demose1a2.html?showinfo=346'>Fractal</A> [Demos - Tech Demo]<BR>
+186. <A HREF='demos7d36.html?showinfo=349'>FracZoom</A> [Demos - Tech Demo]<BR>
+187. <A HREF='demos70f7.html?showinfo=1304'>freemirror</A> [Games - Tool]<BR>
+188. <A HREF='demos93db.html?showinfo=596'>FreeTune Advance 1.0</A> [Games - Tool]<BR>
+189. <A HREF='demosd741.html?showinfo=375'>Frenetic_Voxel</A> [Demos - Tech Demo]<BR>
+190. <A HREF='demosd424.html?showinfo=1410'>Frogger</A> [Games - Classic]<BR>
+191. <A HREF='demosd42b.html?showinfo=419'>Frogger</A> [Games - Classic]<BR>
+192. <A HREF='demos7bab.html?showinfo=218'>Frood</A> [Demos - Tech Demo]<BR>
+193. <A HREF='demos89ee.html?showinfo=480'>FruitGBA</A> [Games - Classic]<BR>
+194. <A HREF='demos4a9b.html?showinfo=382'>FruitLand</A> [Games - Classic]<BR>
+195. <A HREF='demos94db.html?showinfo=548'>Fuerza Aerea</A> [Games - Shootem Up]<BR>
+196. <A HREF='demos9322.html?showinfo=687'>Fuerza Area 2 (Alpha)</A> [Demos - Tech Demo]<BR>
+197. <A HREF='demos9363.html?showinfo=646'>Gadgets</A> [Games - Puzzle]<BR>
+198. <A HREF='demose8b8.html?showinfo=232'>Galaxy Invader 1000</A> [Games - Shootem Up]<BR>
+199. <A HREF='demos7440.html?showinfo=575'>Galaxy Invader 1000 v2</A> [Games - Arcade]<BR>
+200. <A HREF='demoscb18.html?showinfo=1375'>GameBoy Advance GORF</A> [Games - Arcade]<BR>
+<BR><A HREF='demosb64a.html?listby=name&amp;offset=100'>PREVIOUS</A> - <A HREF="demos8ea7.html?listby=name&amp;offset=200">NEXT</A>
+<BR></TD>
+</TR>
+</TABLE></TD>
+<TD VALIGN='TOP' WIDTH='190' BGCOLOR='#FFFFFF'>
+	<TABLE WIDTH='190' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+	<FONT FACE='verdana' SIZE='2'><B>Top Games</B></FONT><BR></TD>
+</TR>
+<TR>
+<TD ALIGN='LEFT' BGCOLOR='#CDCDCD'>
+
+<IMG SRC='demos/2004Mbit_logo.png' WIDTH='184' HEIGHT='123' ALT=''><BR>1. <A HREF='demosff89.html?showinfo=1271'>2004Mbit Compo Game</A><BR>2. <A HREF='demos8c1f.html?showinfo=434'>Xmen V StreetFighter J Edition</A><BR>3. <A HREF='demosb964.html?showinfo=1279'>Another World GBA v2.1</A><BR>4. <A HREF='demos34fb.html?showinfo=1353'>MarioBreak!</A><BR>5. <A HREF='demos1485.html?showinfo=1354'>FF4 Advance- The Return of Golbez</A><BR></FONT></TD>
+</TR>
+</TABLE>	<br>
+	<TABLE WIDTH='190' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+	<FONT FACE='verdana' SIZE='2'><B>Top Demos</B></FONT><BR></TD>
+</TR>
+<TR>
+<TD ALIGN='LEFT' BGCOLOR='#CDCDCD'>
+
+<IMG SRC='demos/SmashBrosAdvance.png' WIDTH='184' HEIGHT='123' ALT=''><BR>1. <A HREF='demosf3bc.html?showinfo=1275'>Smash Bros. Advance</A><BR>2. <A HREF='demos4f79.html?showinfo=472'>Half Life</A><BR>3. <A HREF='demos7150.html?showinfo=1332'>Jespa3D Engine ver 1.39</A><BR>4. <A HREF='demos9e74.html?showinfo=1276'>BulletGBA 5.0</A><BR>5. <A HREF='demos628a.html?showinfo=1329'>Final Fantasy 4th World Cup won by Italy</A><BR></FONT></TD>
+</TR>
+</TABLE>	<br>
+	<TABLE WIDTH='190' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+	<FONT FACE='verdana' SIZE='2'><B>Search</B></FONT><BR></TD>
+</TR>
+<TR>
+	<TD ALIGN='CENTER' VALIGN='CENTER' BGCOLOR='#CDCDCD'>
+	<FORM METHOD='POST' ACTION='http://gbadev.org/search.php'>
+	<INPUT TYPE='hidden' NAME='search_section' VALUE='All'>
+	<INPUT TYPE='text' NAME='search_input' MAXLENGTH='30' SIZE='14'>
+	<INPUT TYPE='submit' VALUE='Search'></FORM>
+	</FONT></TD>
+</TR>
+</TABLE>	<br>
+	<TABLE WIDTH='190' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+	<FONT FACE='verdana' SIZE='2'><B>GBADEV.ORG Stats</B></FONT><BR></TD>
+</TR>
+<TR>
+<TD ALIGN='LEFT' BGCOLOR='#CDCDCD'>
+
+
+<B>Visitors:</B><BR>
+<B>Days Online:</B> 6784<BR>
+
+<B>Articles:</B> 550<BR><B>Games:</B> 388<BR><B>Demos:</B> 237<BR><B>Tools:</B> 176<BR>
+</FONT></TD>
+</TR>
+</TABLE>	<br>
+	<TABLE WIDTH='190' STYLE='BORDER: 1px solid black;' CELLSPACING='0' CELLPADDING='2'>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#0099FF' STYLE='BORDER-RIGHT: hidden; BORDER-BOTTOM: 1px solid black; BORDER-LEFT: hidden;'>
+	<FONT FACE='verdana' SIZE='2'><B>Forum Stats</B></FONT><BR></TD>
+</TR>
+<TR>
+	<TD ALIGN='LEFT' BGCOLOR='#CDCDCD'>
+<B>Top Posters:</B><BR>
+
+1. <A HREF='http://forum.gbadev.org/profile.php?mode=viewprofile&amp;u=180'>tepples</A> (12198)<BR>2. <A HREF='http://forum.gbadev.org/profile.php?mode=viewprofile&amp;u=146'>sgeos</A> (2421)<BR>3. <A HREF='http://forum.gbadev.org/profile.php?mode=viewprofile&amp;u=5846'>HyperHacker</A> (2404)<BR>4. <A HREF='http://forum.gbadev.org/profile.php?mode=viewprofile&amp;u=1120'>sajiimori</A> (2226)<BR>5. <A HREF='http://forum.gbadev.org/profile.php?mode=viewprofile&amp;u=2686'>keldon</A> (2158)<BR><BR><B>Members:</B> 11811<BR><B>Articles:</B> 175447<BR>
+</FONT></TD>
+</TR>
+</TABLE></TD>
+</TR>
+</TABLE>
+</FONT>
+</BODY>
+
+<!-- Mirrored from gbadev.org/demos.php?listby=name&offset=150 by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 05 Jan 2019 23:07:44 GMT -->
+</HTML>
